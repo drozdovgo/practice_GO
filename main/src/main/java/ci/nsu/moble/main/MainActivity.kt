08ed4fd37376a798a158b728a7ec31d8605a2b84
drozdovgo <drozdovgo@mer.ci.nsu.ru>
2026-02-13 14:09:01 +0700

    small refactor

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -1,47 +1,105 @@
 package ci.nsu.moble.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ci.nsu.moble.main.ui.theme.PracticeTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ci.nsu.moble.main.ui.theme.*
+
+private val colorsMap = mapOf(
+    "Red" to Red,
+    "Orange" to Orange,
+    "Yellow" to Yellow,
+    "Green" to Green,
+    "Blue" to Blue,
+    "Indigo" to Indigo,
+    "Violet" to Violet
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Main()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Main() {
+    var input by remember { mutableStateOf("") }
+    val buttonColor = remember { mutableStateOf(Color.Transparent) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PracticeTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(
+            value = input,
+            onValueChange = { input = it},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = {
+                val color = colorsMap[input.trim()]
+                if (color != null) {
+                    buttonColor.value = color
+                } else {
+                    Log.d("ColorSearch", "Цвет \"$input\" не найден")
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = if (buttonColor.value == Color.Transparent)
+                    MaterialTheme.colorScheme.primary
+                else
+                    buttonColor.value
+            )
+
+        ) {
+            Text("Применить цвет")
+
+        }
+
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            items(colorsMap.toList()) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(it.second)
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = it.first, fontSize = 16.sp)
+                    Box(
+                        modifier = Modifier
+                            .size(24.dp)
+                            .background(it.second)
+                    )
+                }
+            }
+        }
     }
 }
